d0859a5493e7fc012acde09c73235a2094253c52
miqqo <mikko291@student.liu.se>
2016-04-05 15:54:18 +0200

    start of making drop downs to item list

---
+++ app/src/main/java/com/example/micke/myapplication/ChildViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.micke.myapplication;
+
+/**
+ * Created by mikaela on 16-04-05.
+ */
+public class ChildViewHolder  {
+    private String title = "";
+
+    public ChildViewHolder(String title){
+        super();
+        this.title = title;
+    }
+}
