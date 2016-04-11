04ac4f2e60832ff7d40c5ce69d12dea65c948b86
Gabriella Rydenfors <gabry944@student.liu.se>
2016-04-11 10:00:26 +0200

    not working, trying to create map fragment

--- app/src/main/java/com/example/micke/myapplication/ListActivity.java
+++ app/src/main/java/com/example/micke/myapplication/ListActivity.java
@@ -1,12 +1,14 @@
 package com.example.micke.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
@@ -48,15 +50,18 @@ class ipAdapter extends RecyclerView.Adapter<ipAdapter.ViewHolder> {
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public final View mView;
         public final TextView mContentView;
+        public final ImageView mMapIconView;
+        public PointOfInterest mPoint;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
             mContentView = (TextView) view.findViewById(R.id.title);
+            mMapIconView = (ImageView) view.findViewById(R.id.goToMapImage);
         }
     }
 
@@ -79,10 +84,33 @@ class ipAdapter extends RecyclerView.Adapter<ipAdapter.ViewHolder> {
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
+        holder.mPoint = ipDataset[position];
         holder.mContentView.setText(ipDataset[position].titel);
+
+        holder.mMapIconView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle arguments = new Bundle();
+                arguments.putString(IndoorMapFragment.ARG_ITEM_ID, holder.mPoint.getID() + "");
+                arguments.putString(IndoorMapFragment.ARG_ITEM_TITLE, holder.mPoint.titel);
+                arguments.putString(IndoorMapFragment.ARG_ITEM_CATEGORY, holder.mPoint.category );
+                arguments.putString(IndoorMapFragment.ARG_ITEM_DESCRIPTION, holder.mPoint.description);
+                arguments.putString(IndoorMapFragment.ARG_ITEM_LATITUDE, holder.mPoint.latitude + "");
+                arguments.putString(IndoorMapFragment.ARG_ITEM_LONGITUDE, holder.mPoint.longitude + "");
+                arguments.putString(IndoorMapFragment.ARG_ITEM_FLOOR, holder.mPoint.floor + "");
+
+                IndoorMapFragment fragment = new IndoorMapFragment();
+                fragment.setArguments(arguments);
+
+                //getSupportFragmentManager().beginTransaction()
+                //        .add(R.id.item_detail_container, fragment)
+                //        .commit();
+            }
+        });
 
     }
 
