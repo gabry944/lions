ce25808b5ed88e15b1cff48b2a6f259ecbfdc9aa
MichaelSjostrom <micke_moj@hotmail.com>
2016-04-11 10:58:34 +0200

    added expanding functionality for items in recyclerview

--- app/src/main/java/com/example/micke/myapplication/ListActivity.java
+++ app/src/main/java/com/example/micke/myapplication/ListActivity.java
@@ -1,12 +1,18 @@
 package com.example.micke.myapplication;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
@@ -57,6 +63,7 @@ class ipAdapter extends RecyclerView.Adapter<ipAdapter.ViewHolder> {
             super(view);
             mView = view;
             mContentView = (TextView) view.findViewById(R.id.title);
+
         }
     }
 
@@ -79,10 +86,50 @@ class ipAdapter extends RecyclerView.Adapter<ipAdapter.ViewHolder> {
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.mContentView.setText(ipDataset[position].titel);
+
+        //To expand an "item" in the recyclerview
+        holder.mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+
+                v.measure(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT);
+                final int targetHeight = v.getMeasuredHeight();
+
+                Log.d("TAG", Integer.toString(v.getMeasuredHeight()));
+
+                // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+                v.getLayoutParams().height = 1;
+                v.setVisibility(View.VISIBLE);
+
+                Animation a = new Animation() {
+                    @Override
+                    protected void applyTransformation(float interpolatedTime, Transformation t) {
+                        //Check to make it expanding more "adaptive way".
+                        /*v.getLayoutParams().height = interpolatedTime == 1
+                                ? RecyclerView.LayoutParams.WRAP_CONTENT
+                                : (int) (targetHeight * interpolatedTime);*/
+                        v.getLayoutParams().height = (int) (targetHeight * interpolatedTime * 10);
+
+                        Log.d("TAG", "2: " + Integer.toString(v.getLayoutParams().height));
+
+                        v.requestLayout();
+                    }
+
+                    @Override
+                    public boolean willChangeBounds() {
+                        return false;
+                    }
+                };
+
+                // 1dp/ms
+                a.setDuration(((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density)) * 4);
+                v.startAnimation(a);
+            }
+        });
 
     }
 
