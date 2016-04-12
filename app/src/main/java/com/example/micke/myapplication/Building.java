004410ccfb66c130104ba78b1e247110deb33ec1
iSirux <joelssonsimon@gmail.com>
2016-04-12 16:41:18 +0200

    Added outdoor firebase support for buildings

--- app/src/main/java/com/example/micke/myapplication/Building.java
+++ app/src/main/java/com/example/micke/myapplication/Building.java
@@ -4,4 +4,29 @@ package com.example.micke.myapplication;
  * Created by iSirux on 2016-04-12.
  */
 public class Building {
+
+    private String name;
+    private String id;
+
+    public Building() {
+
+    }
+
+    public Building(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Building(Building building) {
+        this.name = building.getName();
+        this.id = building.getId();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
