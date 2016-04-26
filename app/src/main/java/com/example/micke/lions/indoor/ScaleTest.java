90c0c0f024ed2a6388e97c98118a21ca36f424df
iSirux <joelssonsimon@gmail.com>
2016-04-26 15:29:05 +0200

    Added right coordinates calculation on imagevie

--- app/src/main/java/com/example/micke/lions/indoor/ScaleTest.java
+++ app/src/main/java/com/example/micke/lions/indoor/ScaleTest.java
@@ -1,17 +1,14 @@
 package com.example.micke.lions.indoor;
 
-import android.app.DialogFragment;
 import android.content.Context;
-import android.graphics.PointF;
 import android.graphics.drawable.BitmapDrawable;
-import android.os.Bundle;
-import android.support.v4.view.ScaleGestureDetectorCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -27,17 +24,14 @@ public class ScaleTest extends RelativeLayout {
     private GestureDetector mGestureDetector;
     private RelativeLayout relativeLayout;
     private ImageView imageView;
-    private View mView;
     private float mX, mY;
+    private float imageWidth, imageHeight, imageYOffset;
+    private IndoorMapFragment mIndoorMapFragment;
 
     public ScaleTest(Context context, AttributeSet attrs) {
         super(context, attrs);
-//        mView = new View(context, attrs);
-//        imageView = new ImageView(context, attrs);
-//        imageView = (ImageView) findViewById(R.id.map);
         mScaleDetector = new ScaleGestureDetector(getContext(), new ScaleListener());
         mGestureDetector = new GestureDetector(getContext(), new PanListener());
-        Log.d("scale", "constructed with context/attrs");
         init();
     }
 
@@ -46,41 +40,24 @@ public class ScaleTest extends RelativeLayout {
         this.imageView = (ImageView) findViewById(R.id.imageView);
         mX = mY = 0;
 
-//        imageView.setOnTouchListener(new OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                final int action = event.getAction();
-//                switch (action & MotionEvent.ACTION_MASK) {
-//                    case MotionEvent.ACTION_DOWN:
-//                        mX = event.getX();
-//                        mY = event.getY();
-//                        break;
-//                }
-//                return false;
-//            }
-//        });
-
-//        imageView.setOnLongClickListener(new View.OnLongClickListener() {
-//            public boolean onLongClick(View arg0) {
-//                Log.d("scale", "Long click @ " + mX + " " + mY);
-////                    addPoint(r, mx - r.getWidth() / 2,
-////                            my - r.getHeight() / 2 + 60);
-//
-////                    float[] point = new float[2];
-////                    point[0] = mx - r.getWidth() / 2;
-////                    point[1] = my - r.getHeight() / 2 + 60;
-////                    DialogFragment newFragment = new AddPointDialogFragment();
-////                    Bundle bundle = new Bundle();
-////                    bundle.putFloat("lat", point[0]);
-////                    bundle.putFloat("lng", point[1]);
-////                    newFragment.setArguments(bundle);
-////                    newFragment.show(indoorActivity.getFragmentManager(), "add_point_layout");
-//                return false;
-//            }
-//        });
+        //Get measurements of image
+        ViewTreeObserver vto = imageView.getViewTreeObserver();
+        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            public boolean onPreDraw() {
+                imageView.getViewTreeObserver().removeOnPreDrawListener(this);
+                imageHeight = imageView.getMeasuredHeight();
+                imageWidth = imageView.getMeasuredWidth();
+                int[] viewCoords = new int[2];
+                imageView.getLocationOnScreen(viewCoords);
+                imageYOffset = viewCoords[1];
+                Log.d("touch", "onPreDraw, imageYOffset: " + imageYOffset);
+                return true;
+            }
+        });
     }
 
     public boolean onTouchEvent(MotionEvent event) {
+//        Log.d("touch", "onTouchEvent!");
         //Get coordinates for finger
         mX = event.getX();
         mY = event.getY();
@@ -97,6 +74,10 @@ public class ScaleTest extends RelativeLayout {
 
     public void setParent(RelativeLayout relativeLayout) {
         this.relativeLayout = relativeLayout;
+    }
+
+    public void setCallback(IndoorMapFragment indoorMapFragment) {
+        mIndoorMapFragment = indoorMapFragment;
     }
 
     private class ScaleListener
@@ -119,14 +100,42 @@ public class ScaleTest extends RelativeLayout {
     private class PanListener
             extends GestureDetector.SimpleOnGestureListener {
 
+        float[] point = new float[2];
+        int[] viewCoords = new int[2];
+        float touchX, touchY;
+        float imageX, imageY;
+        float top, left;
+        float translationX, translationY;
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            //Get the image location on screen (top/left)
+            imageView.getLocationOnScreen(viewCoords);
+
+            //Touched point on screen
+            touchX = e.getX();
+            touchY = e.getY();
+
+            //Touched point on imageview in pixels
+            imageX = (touchX - viewCoords[0]) / relativeLayout.getScaleX();
+            imageY = (touchY - viewCoords[1] + imageYOffset) / relativeLayout.getScaleY();
+
+            //Normalize value - min 0, max 1
+            point[0] = Math.max(0f, Math.min(1f, imageX / imageWidth));
+            point[1] = Math.max(0f, Math.min(1f, imageY / imageHeight));
+
+            //Add a point on the image at touched point
+//            mIndoorMapFragment.showAddPointDialog(point);
+        }
+
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2,
                                 float distanceX, float distanceY) {
-            float top = relativeLayout.getTranslationY();
-            float left = relativeLayout.getTranslationX();
+            top = relativeLayout.getTranslationY();
+            left = relativeLayout.getTranslationX();
 
-            float translationX = left - distanceX;
-            float translationY = top - distanceY;
+            translationX = left - distanceX;
+            translationY = top - distanceY;
 
             relativeLayout.setTranslationX(translationX);
             relativeLayout.setTranslationY(translationY);
