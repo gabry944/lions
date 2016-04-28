66da6bb84173b98eb734b3f616ebb5162196d8cd
iSirux <joelssonsimon@gmail.com>
2016-04-28 11:26:11 +0200

    Removed y offset temp.

--- app/src/main/java/com/example/micke/lions/indoor/MapImage.java
+++ app/src/main/java/com/example/micke/lions/indoor/MapImage.java
@@ -172,6 +172,8 @@ public class MapImage extends RelativeLayout {
         point[0] = latitude * relativeLayout.getScaleX() * imageWidth;
         point[1] = longitude * relativeLayout.getScaleY() * imageHeight + imageYOffset;
 
+        point[1] = longitude * relativeLayout.getScaleY() * imageHeight;
+
         Log.d("touch", "x: " + point[0] + " latitude: " + latitude + " xscale: " + relativeLayout.getScaleX()
                 + " imagewidth: " + imageWidth + " viewcoords[0]: " + viewCoords[0]);
 
