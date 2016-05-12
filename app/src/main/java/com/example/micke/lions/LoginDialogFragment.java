06c88acaa383898517b945eff8bac4b2c99fead7
santr006 <santr006@student.liu.se>
2016-05-12 11:43:54 +0200

    changed name LoginDialogFragment2 to LoginDialogFragment

--- app/src/main/java/com/example/micke/lions/LoginDialogFragment.java
+++ app/src/main/java/com/example/micke/lions/LoginDialogFragment.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class LoginDialogFragment2 extends DialogFragment {
+public class LoginDialogFragment extends DialogFragment {
 
     private IndoorActivity indoorActivity = null;
     private OutdoorActivity outdoorActivity = null;
